beed1055e0836abe5861dbb14a61f5c8ae3e6938
rq-sasakix69-at-321899771753 <sasakix69@gmail.com>
2022-06-08 19:41:38 +0900

    Fix: Webpackerをコンパイルさせるためにgemを削除

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,10 +1,3 @@
 const { environment } = require('@rails/webpacker')
 
 module.exports = environment
-
-module.exports = {
-  performance: {
-    maxEntrypointSize: 500000,
-    maxAssetSize: 500000,
-  }
-}
